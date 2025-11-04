3dbc8c305157b230fd694cbbd55b8117e0de6540
bubksrit <sehgalkriti2007@gmail.com>
2025-11-04 19:51:37 +0530

    Add pyramid pattern generator in C++
    Introduces p1.cpp, which prints a numeric pyramid pattern, and includes the compiled executable p1.exe.

---
+++ p1.cpp
@@ -0,0 +1,17 @@
+ #include<iostream>
+using namespace std;
+int main(){
+    int n=6;
+    int up;
+    for (int i=1;i<n;i++){
+        for(int j=n;j>i;j--)
+        cout<<" ";
+    for (int j=1;j<=i;j++){
+    cout<<j;
+    up=j;}
+    for(int k=up-1;k>0;k--)
+    cout<<k;
+      cout<<endl;
+    }
+    return 0;
+}
