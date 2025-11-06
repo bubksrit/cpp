3270afc4cae7e0fef4b6f341852303793dff8a41
bubksrit <sehgalkriti2007@gmail.com>
2025-11-06 18:10:02 +0530

    Add second pattern printing loop to p3.cpp
    Introduced a new loop to print an additional pattern after the existing one. Also fixed the first loop to iterate up to n instead of n-1 for correct output.

--- p3.cpp
+++ p3.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main(){
     int n=4;
-    for (int i=1;i<n;i++){
+    for (int i=1;i<=n;i++){
         for(int j=n;j>i;j--){
         cout<<" ";}
         if (i>1)
@@ -16,5 +16,21 @@ int main(){
      cout<<endl;
 
     }
+    
+    for (int i=1;i<=n;i++){
+        for(int j=0;j<i;j++){
+        cout<<" ";}
+        if (i<n-1)
+        cout<<"*";
+        for(int j=(2*n-1);j>(2*i);j--)
+        {
+        cout<<" ";
+        }
+        if (i<n)
+      cout<<"*";
+     cout<<endl;
+
+    }
+    
     return 0;
 }
